c8e9b57498fe300d6b67eeb96efc5641522c1e3f
Karina Nichiporuk <nichiporuk.karina@yandex.ru>
2023-07-27 18:51:35 +0400

    15 project ревью

---
+++ frontend/src/utils/Api.js
@@ -0,0 +1,124 @@
+class Api {
+  constructor(options) {
+    this._baseUrl = options.baseUrl
+  }
+
+  // Формирую запрос на сервер, если прошел не удачно, возвращаем ошибку!
+  _handleSendingRequest(res) {
+    if (res.ok) {
+      return Promise.resolve(res.json())
+    }
+
+    // Если ошибка пришла, отклоняем промис
+    return Promise.reject(`Ошибка: ${res.status}`)
+  }
+
+  // Метод загрузки информации о пользователе с сервера
+  async getRealUserInfo() {
+    const response = await fetch(`${this._baseUrl}/users/me`, {
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      },
+    })
+    return this._handleSendingRequest(response)
+  }
+
+  //Метод загрузки карточек с сервера
+  async getCards() {
+    const response = await fetch(`${this._baseUrl}/cards`, {
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      },
+    })
+    return this._handleSendingRequest(response)
+  }
+
+  // Метод редактирование профиля
+  async editProfileUserInfo(data) {
+    const response = await fetch(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      },
+      body: JSON.stringify({
+        name: data.name,
+        about: data.about,
+      }),
+    })
+    return this._handleSendingRequest(response)
+  }
+
+  //Метод добавления новой карточки
+  async getNewCard(data) {
+    const response = await fetch(`${this._baseUrl}/cards`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      },
+      body: JSON.stringify(data),
+    })
+    return this._handleSendingRequest(response)
+  }
+
+  // Метод постановки лайка карточки
+  async addLike(cardId) {
+    const response = await fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      },
+    })
+    return this._handleSendingRequest(response)
+  }
+
+  // Метод удаления карточки
+  async removeCard(cardId) {
+    const response = await fetch(`${this._baseUrl}/cards/${cardId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      },
+    })
+    return this._handleSendingRequest(response)
+  }
+
+  // Метод постановки и снятия лайка с карточки
+  async removeLike(cardId) {
+    const response = await fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      },
+    })
+    return this._handleSendingRequest(response)
+  }
+
+  // Метод обновления аватара пользователя
+  async updateProfileUserAvatar(data) {
+    const response = await fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      },
+      body: JSON.stringify({
+        avatar: data.avatar,
+      }),
+    })
+    return this._handleSendingRequest(response)
+  }
+}
+
+const api = new Api({
+  baseUrl: "https://api.karina.nomoreparties.sbs",
+  // baseUrl: "http://localhost:3000",
+})
+
+export default api
